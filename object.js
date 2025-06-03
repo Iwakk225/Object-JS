b5f5941ee8c02a1828ebe4fef81d36d70d50a954
Iwakk225 <muhammadadityarahmansyah18@gmail.com>
2025-06-03 09:21:26 +0700

    belajar object literal

---
+++ object.js
@@ -0,0 +1,18 @@
+// membuat object
+// 1. object literal
+
+var siswa1 = {
+    nama : "Muhammad Aditya Rahmansyah",
+    kelas : "XI RPL 1",
+    umur : 18,
+    jurusan : "Rekayasa Perangkat Lunak",
+    email : "muhammadadityarahmansyah18@gmail.com"
+}
+
+var siswa2 = {
+    nama : "Raditya Nala",
+    kelas : "XI RPL 1",
+    umur : 18,
+    jurusan : "Rekayasa Perangkat Lunak",
+    email : "radityanala316@gmail.com"
+}
